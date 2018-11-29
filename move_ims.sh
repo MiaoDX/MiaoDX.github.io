8c695dd578206b5fc948b106604e6b758ba70a76
miaodx <miaodx@hotmail.com>
2018-11-29 14:18:11 +0800

    add change images paths function

---
+++ move_ims.sh
@@ -0,0 +1,16 @@
+for d in $(find ./source/_posts/blog_source -maxdepth 2 -type d)
+do
+	#Do something, the directory is accessible with $d:
+	bn=$(basename $d)
+	if [ $bn = pics ]
+	then echo $d
+		for f in $(find $(dirname $d) -maxdepth 2 -type f -name '*.md')
+		do
+			echo $f
+			f_d="${f%.*}"
+			mkdir -p $f_d
+			cp $d $f_d -r
+			echo $f_d
+		done
+	fi
+done
